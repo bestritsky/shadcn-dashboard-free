fdf17b2a060791aa010fa8f890ad2009faff07ab
bestritsky <145217170+bestritsky@users.noreply.github.com>
2025-05-14 23:46:25 -0400

    Fix SWC and Jest worker errors

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ config();
 
 const nextConfig = {
   swcMinify: false,
+  experimental: {
+    swcMinify: false,
+    swcLoader: false
+  },
   env: {
     DASHBOARD_BASE_URL: process.env.DASHBOARD_BASE_URL
   }
